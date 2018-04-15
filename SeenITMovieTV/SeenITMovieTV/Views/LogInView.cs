bdbe68ee6ac19e6c191c2a2e4f96cdb2fe7e5313
TheSynthCity <aaronm2006@hotmail.co.uk>
2018-04-15 19:19:32 +0100

    Added lost credentials functionality

--- SeenITMovieTV/SeenITMovieTV/Views/LogInView.cs
+++ SeenITMovieTV/SeenITMovieTV/Views/LogInView.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using MetroFramework.Forms;
 using System.Windows.Forms;
 using SeenITMovieTV.Database;
+using System.Net.Mail;
+using System.Net;
 
 namespace SeenITMovieTV.Views
 {
@@ -25,7 +27,7 @@ namespace SeenITMovieTV.Views
 
         private void LogIn_Button_Click(object sender, EventArgs e)
         {
-            bool success = DataConnection.GetUserLogin(UserName_MaskedTextBox.Text, Password_MaskedTextBox.Text);
+            bool success = DataConnection.GetUserLogin(UserName_MaskedTextBox.Text);
 
             if (success == true)
             {
@@ -43,16 +45,17 @@ namespace SeenITMovieTV.Views
 
         private void Create_Account_Button_Click(object sender, EventArgs e)
         {
-            if (UserName_MaskedTextBox.Text == string.Empty || Password_MaskedTextBox.Text == string.Empty)
+            if (UserName_MaskedTextBox.Text == string.Empty || Password_MaskedTextBox.Text == string.Empty || Email_MaskedTextbox.Text == string.Empty)
             {
                 MessageBox.Show("Error! Missing either Username OR Password");
                 UserName_MaskedTextBox.ResetText();
                 Password_MaskedTextBox.ResetText();
+                Email_MaskedTextbox.ResetText();
 
                 return;
             }
 
-            bool success = DataConnection.CreateUserLogin(UserName_MaskedTextBox.Text, Password_MaskedTextBox.Text);
+            bool success = DataConnection.CreateUserLogin(UserName_MaskedTextBox.Text, Password_MaskedTextBox.Text, Email_MaskedTextbox.Text);
 
             if(success == true)
             {
@@ -70,6 +73,38 @@ namespace SeenITMovieTV.Views
         private void LogInView_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void Forgot_Password_Label_Click(object sender, EventArgs e)
+        {
+            if(Email_MaskedTextbox.Text == string.Empty)
+            {
+                MessageBox.Show("Please input your email address to recover your login details");
+            }
+            else
+            {
+                List<string> FoundDetails = DataConnection.RetrieveLostDetails(Email_MaskedTextbox.Text);
+
+                if (FoundDetails.Count != 0)
+                {
+                    var message = new MailMessage("seenitmovieseries@gmail.com", Email_MaskedTextbox.Text);
+
+                    message.Subject = "SeenIT - Login Credentials [Contains Sensitive Data]";
+
+                    message.Body = "Hi, you've recently requested that we email you your login credentials for our application. Please Find below your details: \n\n Username:"+FoundDetails[0]+" \n Password: "+FoundDetails[1]+"\n \n Thankyou.";
+
+                    SmtpClient mailer = new SmtpClient("smtp.gmail.com", 587);
+                    mailer.Credentials = new NetworkCredential("YOUREMAIL@gmail.com", "YOURPASSWORD");
+                    mailer.EnableSsl = true;
+                    mailer.Send(message);
+
+                    MessageBox.Show("We've sent an email to " + Email_MaskedTextbox.Text + " containing your details");
+                }
+                else
+                {
+                    MessageBox.Show("Error: No login details associated with that email have been found");
+                }
+            }
         }
     }
 }
